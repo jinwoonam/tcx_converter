ef202a8629ff18cf1a7b97e56675cecaafeac7c5
jinwoonam <jinwoonam@empal.com>
2019-07-12 14:32:18 +0900

    Create get_segment.py

---
+++ get_segment.py
@@ -0,0 +1,81 @@
+import requests
+import sys
+import datetime
+
+# https://www.strava.com/settings/api
+strava_token = "Bearer 49b43c26085dce9a7fb39142b7c029d37a728cb5"
+
+if (2 == len(sys.argv)):
+    seg_no = sys.argv[1]
+else:
+    print("Usage : {py} segment_number".format(py=sys.argv[0]))
+    exit(0)
+
+url = "https://www.strava.com/api/v3/segments/" + seg_no
+
+headers = {
+    'Authorization': strava_token,
+    'User-Agent': "PostmanRuntime/7.15.0",
+    'Accept': "*/*",
+    'Cache-Control': "no-cache",
+    'Postman-Token': "918a31ce-671f-4665-8487-d34cd41d202f,356930ab-1ac0-4478-80e0-150922f50d16",
+    'Host': "www.strava.com",
+    'cookie': "_strava4_session=a1kcv8lbe0i7rlukjdt9kvnfo0viif8d",
+    'accept-encoding': "gzip, deflate",
+    'Connection': "keep-alive",
+    'cache-control': "no-cache"
+    }
+
+r = requests.request("GET", url, headers=headers).json()
+#print(r.text)
+
+''' Sample 
+{"id":8306671,"resource_state":3,"name":"계파로 오픈구간","activity_type":"Ride",
+ "distance":12801.7,"average_grade":0.4,"maximum_grade":46.9,
+ "elevation_high":316.5,"elevation_low":134.8,
+ "start_latlng":[38.066698,127.657155],"end_latlng":[38.161143,127.639063],
+ "start_latitude":38.066698,"start_longitude":127.657155,
+ "end_latitude":38.161143,"end_longitude":127.639063,"climb_category":0,
+ "city":"Hwacheon-gun","state":"Gangwon-do","country":"South Korea",
+ "private":false,"hazardous":false,"starred":false,
+ "created_at":"2014-10-09T12:50:11Z","updated_at":"2019-07-10T08:19:53Z",
+ "total_elevation_gain":617.8,
+ "map":{"id":"s8306671","polyline":"y{igFe`djWUNe@Ji@f@QJMNOHSFa@\\SJs@Xs@j@WNo@n@iFhDWV}B~@}@VeAd@_@^uBpA_@L]V_@RsAnA{@lA[Zk@x@[VU^WPeAdAm@z@q@n@kAvA[V_ClA_A\\u@d@cAX_Ab@c@Jc@Bc@EcB}@YSSWUQSUa@M[Eg@BQFs@d@WJYBiCBkAAODk@CUI_@?QFSGUFS?k@ISIMSQi@Kk@O[EOSMOK]@e@XMRGXMVQTQHSDUESB_@IM@MEUAOCODe@Cm@PMJ_@AQI_@[I{@AOBKGa@B[Ig@GGCQIGq@UK?KBk@AIJMCa@FUTEG_@PUNQVe@AYKMCIBWGMBWRKLQh@EH_@LMPOJc@DMJODMJOPKTw@dAKFQFSBYf@a@rAMVQJBJIEMRYDYNWFq@Fw@Bq@ZYx@I~@?r@Sb@s@Dq@g@u@e@eEz@gBb@kBrA}AbAyAOoAiAgAmA}@k@eBe@oBQ_FMeB@aBL{Ag@kAaAe@cBeDiCwBoCk@mA_AeDSwASwCUkAa@kA[kAQiA[cAoAsBo@e@i@Yk@Ue@Wi@Se@Ke@OgAA]JYPa@h@SNWVq@\\o@b@UVQ\\SXQ^Od@SZ[Z]RYF]R_@J_@D]V[Jy@j@iAbA]TUXo@b@UXYRQTOJORg@f@SXs@pAOZo@tBMVUJURO?@F?CYG_@W_@k@m@gCWgF_@eBYkBMoBEuBq@yAsAAgC^_B^aBToCr@kAv@m@d@eC|C_@jAc@hB[`Ac@fAo@|@]N[R]\\YTiBdC]PWRkAd@iAJi@Lg@HkAZa@PcAp@w@`Ak@~@YVkApCQXYX[P_AZ_@DwBz@wB`@}@h@[L[XWZYRa@FkB@o@Ce@IiAOsFR_ACmDCiDg@oA`@}AvDgAtAkB~AuA`AqD`@eAj@y@Xu@FiAEgAJ_@?]BoAb@y@Rc@ZUBQJQNUD{AAWBmAb@SDSASJQXc@Fe@Aq@PYRm@Pi@@q@ReC`AoAPyADwAo@qAFa@nAS~AoALw@g@g@K{@A[RQBQLWF_@Tm@Vg@DWHc@?_@CK@m@EMNGNS|@IPEPUd@a@TSFs@`@]LMBO?QGa@YQSO_@_@]QWO[_@W{@O_@A{ABaAHiABkBNeBAqBJwBI}BQsBEmDFuAOw@iAq@qAcAo@gA@{@~@k@vAy@vAw@`AaAd@mAEw@Os@[cBsAgAe@eDUiAMiAV}@jAk@|AmAn@kB@uEC","resource_state":3},
+ "effort_count":202,"athlete_count":134,"star_count":0,
+ "athlete_segment_stats":{"pr_elapsed_time":1905,"pr_date":"2019-07-10","effort_count":1}}
+
+  <wpt lat="37.54348845865" lon="127.106323242188">
+    <name>출발 지점</name>
+    <desc>설명추가</desc>
+  </wpt>
+'''
+
+
+# Start Position
+gpx_wpt = ('  <wpt lat="{latt}" lon="{long}">\n'
+	.format(latt=r["start_latitude"], long=r["start_longitude"]))
+gpx_wpt += ('    <name>{cat}_{dist:.1f}_{grade}%</name>\n'
+	.format(cat=5-r["climb_category"], dist=r["distance"]/1000, grade=r["average_grade"]))
+gpx_wpt += ('    <desc>{id}_{name}</desc>\n'.format(id=r["id"], name=r["name"]))
+gpx_wpt += ('  </wpt>\n')
+
+#end Position
+gpx_wpt += ('  <wpt lat="{latt}" lon="{long}">\n'
+	.format(latt=r["end_latitude"], long=r["end_longitude"]))
+gpx_wpt += ('    <name>{name}</name>\n'.format(name=r["name"]))
+gpx_wpt += ('    <desc>{name}</desc>\n'.format(name=r["name"]))
+gpx_wpt += ('  </wpt>\n')
+
+print(gpx_wpt)
+
+
+# apend to file
+# file name : segement_190712.gpx
+now = datetime.datetime.now()
+filename = now.strftime('segment_%y%m%d.gpx')
+#print(filename)
+
+f = open(filename, 'a')
+f.write(gpx_wpt)
+print("\n --> Written to " + filename)
+f.close()
