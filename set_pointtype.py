3dabe2650394c85524ee3645e21a85702fec3d46
jinwoonam <jinwoonam@empal.com>
2019-07-12 19:46:05 +0900

    Add files via upload

--- set_pointtype.py
+++ set_pointtype.py
@@ -1,11 +1,19 @@
 import xml.etree.ElementTree as ET
+import sys
 
-track_name = 'test'
+#parameter = sys.argv[0].split('.')
+if len(sys.argv) == 1:          # 옵션 없으면 도움말 출력하고 종료
+    print("use test.gpx")
+    track_name = 'test'
+else:
+    sys.argv[1]
+    track_name = sys.argv[1].split('.')[0]
+    print("use", sys.argv[1])
 
 ns = {'garmin_tc': 'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2',
       'role': 'http://characters.example.com'}
 
-tree = ET.parse('0713anbandegi.tcx')
+tree = ET.parse(track_name+'.tcx')
 root = tree.getroot()
 
 ET.register_namespace('', 'http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2')
@@ -36,9 +44,11 @@ for courses in root.findall('garmin_tc:Courses', ns):
         if (name.text.lower().find('top') != -1):
             pointtype.text = 'Summit'
             upstart = False
-        if (name.text.lower().find('lunch') != -1) :
+        if (name.text.lower().find('lunch') != -1):
             pointtype.text = 'Food'
-        if ((name.text.lower().find('hanaro') != -1) | (name.text.lower().find('mart') != -1) | (name.text.lower().find('cu') != -1) | (name.text.lower().find('feed') != -1)):
+        if ((name.text.lower().find('hanaro') != -1) | (name.text.lower().find('mart') != -1) | (name.text.lower().find('cvs') != -1) | (name.text.lower().find('feed') != -1)):
             pointtype.text = 'Water'
+        if (name.text.lower().find('open') != -1):
+            pointtype.text = 'Sprint'
 
-tree.write('output.tcx', 'UTF-8', True)
+tree.write(track_name+'_wp.tcx', 'UTF-8', True)
